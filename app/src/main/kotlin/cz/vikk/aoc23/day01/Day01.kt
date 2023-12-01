0f83655dc20944cdec5da53c8c089d0617bb3029
Viktor Krejčíř <krejcirviktor@gmail.ccom>
2023-12-01 10:43:45 +0100

    AOC - using non-hack algorithm

--- app/src/main/kotlin/cz/vikk/aoc23/day01/Day01.kt
+++ app/src/main/kotlin/cz/vikk/aoc23/day01/Day01.kt
@@ -6,16 +6,7 @@ import reactor.core.publisher.Mono
 class Day01 {
 
     private val digitRegex = """([0-9])""".toRegex()
-    private val writtenNumberRegex = """(one|two|three|four|five|six|seven|eight|nine)""".toRegex()
-    private val writtenNumberRegexSolid = """(?=(one|two|three|four|five|six|seven|eight|nine))""".toRegex()
-    private val replacementMap = mapOf(
-        "one" to "o1e", "two" to "t2o", "three" to "th3ree", "four" to "f4r", "five" to "f5e", "six" to "s6x", "seven" to "s7n",
-        "eight" to "e8h", "nine" to "n9e",
-    )
-    private val replacementSolid = mapOf(
-        "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7",
-        "eight" to "8", "nine" to "9",
-    )
+    private val numberArray = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
     fun firstTask(dataLoader: ResourceDataLoader): Mono<Int> {
         return dataLoader.loadLinesFromFileFlux().map { convertToDoubleDigitNumber(it) }.onErrorReturn(0).reduce { t, u -> t + u }
@@ -28,22 +19,52 @@ class Day01 {
 
     fun secondTask(dataLoader: ResourceDataLoader): Mono<Int> {
         return dataLoader.loadLinesFromFileFlux()
-            .map { replaceStringWithNumeralsHack(it) }
-            .map { convertToDoubleDigitNumber(it) }
-           // .doOnEach { println(it) }
+            .map { detectNumbers(it) }
+            .map { createDoubleDigitNumber(it) }
             .onErrorReturn(0)
             .reduce { t, u -> t + u }
     }
 
-    private fun replaceStringWithNumeralsHack(input: String): String {
-        var result = input
-        replacementMap.forEach{ result = result.replace(it.key,it.value,true)}
-        return result
-    }
-    private fun replaceStringWithNumerals(input: String): String {
-         return writtenNumberRegex.replace(input.lowercase()){
-             replacementMap[it.value] + ""
-         }
+    private fun detectNumbers(input: String): List<String> {
+        return numberArray.map {
+            extractNumbers(input, it)
+        }.flatten()
+         .filter {
+                it.position != -1
+         }.sortedBy {
+                it.position
+         }.map {
+                it.convertToDigit()
+         }.toList()
     }
 
+    private fun createDoubleDigitNumber(digits: List<String>): Int {
+        return "${digits.first()}${digits.last()}".toInt()
+    }
+
+    private fun extractNumbers(input: String, number: String): List<NumberWithPosition> {
+        var index = input.indexOf(number, 0, true)
+        var arrayOfPositionedNumbers = mutableListOf<NumberWithPosition>()
+        while (index != -1) {
+            arrayOfPositionedNumbers.add(NumberWithPosition(index, number))
+            val nextIndex = if ((index +1) < input.length ) index +1 else break
+            index = input.indexOf(number, nextIndex, true)
+        }
+        return arrayOfPositionedNumbers
+    }
+}
+
+class NumberWithPosition(val position: Int, val content: String) {
+    private val replacementSolid = mapOf(
+        "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7",
+        "eight" to "8", "nine" to "9",
+    )
+
+    fun convertToDigit(): String {
+        return if (content.lowercase() in replacementSolid.keys) {
+            replacementSolid[content.lowercase()]!!
+        } else {
+            content
+        }
+    }
 }
